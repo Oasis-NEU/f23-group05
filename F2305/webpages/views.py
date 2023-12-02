86a8369230773b51d5f1203dd20360e222ccdba2
Chris-N-Wu <dolphins0248@gmail.com>
2023-12-02 15:13:07 -0500

    multiple dunkin locations + maps

--- F2305/webpages/views.py
+++ F2305/webpages/views.py
@@ -8,11 +8,11 @@ def home(request):
 def webpageDunkin(request):
     print(request.build_absolute_uri()) #optional
 
-    data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20], [2, 39, 37, 87, 88]]
+    data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20], [2, 39, 37, 87, 88], [50, 30, 23, 20, 15]]
     fig = px.imshow(data,
-                    labels=dict(x="Day of Week", y="Time of Day", color="Busyness"),
-                    x=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-                    y=['12pm', '1pm', '2pm', '3pm'],
+                    labels=dict(x="Time of Day", y="Day of Week", color="Customers"),
+                    x=['12pm', '1pm', '2pm', '3pm', '4pm'],
+                    y=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
                 )
     fig.update_xaxes(side="top")
     fig.update_layout({'plot_bgcolor': 'rgba(0, 0, 0, 0)', "paper_bgcolor": 'rgba(0,0,0,0)'})
@@ -20,7 +20,7 @@ def webpageDunkin(request):
     return render(
         request,
         'webpages/dunkin.html',
-        context={'plot': fig.to_html(config={'displayModeBar': False})}
+        context={'plot': fig.to_html(config={'displayModeBar': False}), 'plot2': fig.to_html(config={'displayModeBar': False})}
     )
 
 def webpageStarbucks(request):
