af3548a33dd9809d61ba33835e7374d263fa4310
Chris-N-Wu <dolphins0248@gmail.com>
2023-12-02 15:18:52 -0500

    more data

--- F2305/webpages/views.py
+++ F2305/webpages/views.py
@@ -8,10 +8,14 @@ def home(request):
 def webpageDunkin(request):
     print(request.build_absolute_uri()) #optional
 
-    data=[[1, 25, 30, 50, 1], [20, 1, 60, 80, 30], [30, 60, 1, 5, 20], [2, 39, 37, 87, 88], [50, 30, 23, 20, 15]]
+    data=[[20, 30, 3, 5, 1, 25, 30, 50, 1, 5], 
+          [3, 13, 20, 6, 20, 1, 60, 80, 30, 2], 
+          [49, 40, 33, 97, 30, 60, 1, 5, 20, 3], 
+          [3, 1, 9, 31, 2, 39, 37, 87, 88, 20], 
+          [49, 92, 47, 55, 50, 30, 23, 20, 15, 40]]
     fig = px.imshow(data,
                     labels=dict(x="Time of Day", y="Day of Week", color="Customers"),
-                    x=['12pm', '1pm', '2pm', '3pm', '4pm'],
+                    x=['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'],
                     y=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
                 )
     fig.update_xaxes(side="top")
